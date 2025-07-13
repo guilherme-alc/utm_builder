79ef450ef03ffb9145aead26a8d06c126a14acb2
guilherme-alc <guilhermealc01@gmail.com>
2025-07-13 18:12:18 -0300

    Concatenando as informações com extension methods

--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -1,4 +1,5 @@
-﻿using UtmBuilder.Core.ValueObjects;
+﻿using UtmBuilder.Core.Extensions;
+using UtmBuilder.Core.ValueObjects;
 
 namespace UtmBuilder.Core
 {
@@ -17,5 +18,17 @@ namespace UtmBuilder.Core
         /// Campaign details
         /// </summary>
         public Campaign Campaign { get; init; }
+
+        public override string ToString()
+        {
+            var segments = new List<string>();
+            segments.AddIfNotNull("utm_source", Campaign.Source);
+            segments.AddIfNotNull("utm_medium", Campaign.Medium);
+            segments.AddIfNotNull("utm_campaing", Campaign.Name);
+            segments.AddIfNotNull("utm_id", Campaign.Id);
+            segments.AddIfNotNull("utm_term", Campaign.Term);
+            segments.AddIfNotNull("utm_content", Campaign.Content);
+            return $"{Url.Address}?{string.Join("&", segments)}";
+        }
     }
 }
