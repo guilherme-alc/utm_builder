792fc937f2a6a03b02e622a757461362dd515974
guilherme-alc <guilhermealc01@gmail.com>
2025-07-15 06:38:12 -0300

    DataRow em teste de Url

--- UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -6,39 +6,23 @@ namespace UtmBuilder.Core.Tests.ValueObjects
     [TestClass]
     public class UrlTests
     {
-
+        [DataRow("")]
+        [DataRow("teste")]
+        [DataRow(null)]
         [TestMethod]
-        public void DeveRetornarExcecaoQuandoUrlInvalida()
+        public void DeveLancarExcecaoParaUrlsInvalidas(string urlInvalida)
         {
-            try
-            {
-                new Url("teste");
-                Assert.Fail();
-            } 
-            catch (InvalidUrlException ex)
-            {
-                Assert.IsTrue(true);
-            }
+            Assert.ThrowsException<InvalidUrlException>(() => new Url(urlInvalida), $"URL inválida esperada: {urlInvalida ?? "null"}");
         }
 
         [TestMethod]
-        public void DeveRetornarExcecaoQuandoUrlVazia()
+        [DataRow("https://github.com/guilherme-alc")]
+        [DataRow("http://localhost:5000/api/v1/customer")]
+        [DataRow("http://127.0.0.1:8080/guilherme-alc")]
+        public void DeveAceitarUrlsValidas(string urlValida)
         {
-            Assert.ThrowsException<InvalidUrlException>(() => new Url(""));
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(InvalidUrlException))]
-        public void DeveRetornarExcecaoQuandoUrlNula()
-        {
-            new Url(null);
-        }
-
-        [TestMethod]
-        public void NaoDeveRetornarExcecaoQuandoUrlValida()
-        {
-            var url = new Url("https://github.com/guilherme-alc");
-            Assert.AreEqual("https://github.com/guilherme-alc", url.Address);
+            var u = new Url(urlValida);
+            Assert.AreEqual(urlValida, u.Address, $"Falha ao validar a URL: {urlValida}");
         }
     }
 }
