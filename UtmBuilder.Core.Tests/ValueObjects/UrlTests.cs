33ec7cddc31565e6d0996f25a00e16a75e055d9d
guilherme-alc <guilhermealc01@gmail.com>
2025-07-15 06:26:47 -0300

    Adiciona testes para Url

---
+++ UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -0,0 +1,44 @@
+﻿using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests.ValueObjects
+{
+    [TestClass]
+    public class UrlTests
+    {
+
+        [TestMethod]
+        public void DeveRetornarExcecaoQuandoUrlInvalida()
+        {
+            try
+            {
+                new Url("teste");
+                Assert.Fail();
+            } 
+            catch (InvalidUrlException ex)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod]
+        public void DeveRetornarExcecaoQuandoUrlVazia()
+        {
+            Assert.ThrowsException<InvalidUrlException>(() => new Url(""));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidUrlException))]
+        public void DeveRetornarExcecaoQuandoUrlNula()
+        {
+            new Url(null);
+        }
+
+        [TestMethod]
+        public void NaoDeveRetornarExcecaoQuandoUrlValida()
+        {
+            var url = new Url("https://github.com/guilherme-alc");
+            Assert.AreEqual("https://github.com/guilherme-alc", url.Address);
+        }
+    }
+}
