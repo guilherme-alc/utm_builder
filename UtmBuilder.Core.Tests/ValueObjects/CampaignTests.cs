23a5f34075892a01e1aa46e54dba129944453836
guilherme-alc <guilhermealc01@gmail.com>
2025-07-15 17:43:30 -0300

    Adiciona testes para Campaign

---
+++ UtmBuilder.Core.Tests/ValueObjects/CampaignTests.cs
@@ -0,0 +1,36 @@
+﻿using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
+
+namespace UtmBuilder.Core.Tests.ValueObjects
+{
+    [TestClass]
+    public class CampaignTests
+    {
+        [TestMethod]
+        [DataRow("", "", "", true)]
+        [DataRow("src", "", "", true)]
+        [DataRow("src", "medium", "", true)]
+        [DataRow("src", "medium", "name", false)]
+        public void TestCampaing(string source, string medium, string name, bool esperaExcecao)
+        {
+
+            if (esperaExcecao)
+            {
+                try
+                {
+                    new Campaign(source, medium, name);
+                    Assert.Fail();
+                }
+                catch (InvalidCampaignException)
+                {
+                    Assert.IsTrue(true);
+                }
+            }
+            else
+            {
+                new Campaign(source, medium, name);
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
